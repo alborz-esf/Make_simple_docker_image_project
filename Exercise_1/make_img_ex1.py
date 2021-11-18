408e4d05a54e289fc60624cda3a360393a0461d4
alborz-esf <90214078+alborz-esf@users.noreply.github.com>
2021-11-19 03:06:31 +0330

    Create make_img_ex1.py

---
+++ Exercise_1/make_img_ex1.py
@@ -0,0 +1,11 @@
+# A.Esfandyari
+
+GREEN = '\033[92m'
+RED = '\033[91m'
+END = '\033[0m'
+def first_image():
+  print(GREEN + "--> Hello ....." + END)
+  print(GREEN + "\n   --> This is a docker image ...... " + END)
+  print(RED + "\n    --> ;))) " + END)
+
+first_image()
